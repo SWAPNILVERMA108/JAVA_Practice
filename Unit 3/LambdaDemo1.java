a95b8cd2dbb6acaed4ef42f82ca14cd9e1f3dd8d
SWAPNIL VERMA <swapnilverma109@gmail.com>
2026-04-19 23:51:30 +0530

    java code

--- Unit 3/LambdaDemo1.java
+++ Unit 3/LambdaDemo1.java
@@ -3,6 +3,7 @@ interface NumericTest{
 }
 public class LambdaDemo1 {
     public static void main(String[] args) {
+        
         NumericTest isEven = (n)->(n%2)==0;
 
         if(isEven.test(10))System.out.println("10  is even no ");
